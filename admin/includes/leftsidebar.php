e4626a2dc911119a7c2494ca600d97b37dae4c30
modungphucno1 <tannguyenminh98.1@gmail.com>
2019-06-05 19:26:46 +0700

    admin 1st

---
+++ admin/includes/leftsidebar.php
@@ -0,0 +1,69 @@
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                        	<li class="menu-title">Navigation</li>
+
+                            <li class="has_sub">
+                                <a href="dashboard.html" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                         
+                            </li>
+
+
+
+               
+
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Category </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                	<li><a href="add-category.html">Add Category</a></li>
+                                    <li><a href="manage-categories.html">Manage Category</a></li>
+                                </ul>
+                            </li>
+
+    <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span>Authors </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-author.html">Add Author</a></li>
+                                    <li><a href="manage-author.html">Manage Author</a></li>
+                                </ul>
+                            </li>               
+  <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Posts </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-post.html">Add Posts</a></li>
+                                    <li><a href="manage-posts.html">Manage Posts</a></li>
+                                     
+                                </ul>
+                            </li>  
+                     
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Pages </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="aboutus.html">About us</a></li>
+                                    <li><a href="contactus.html">Contact us</a></li>
+                                </ul>
+                            </li>
+   <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Comments </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                  <li><a href="unapprove-comment.html">Waiting for Approval </a></li>
+                                    <li><a href="manage-comments.html">Approved Comments</a></li>
+                                </ul>
+                            </li>   
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+               
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
